1f1c1bee473654f2cc0d184794409184dbaf00a3
BrunaKimura <brunamk@al.insper.edu.br>
2017-10-23 21:27:39 -0200

    printando o texto

--- 8-Modulacao-Digital/src/Receptor.py
+++ 8-Modulacao-Digital/src/Receptor.py
@@ -1,12 +1,17 @@
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 1234))
+serversocket.bind(('localhost', 1235))
 serversocket.listen(1) 
+
+buf = []
 
 while True:
     connection, address = serversocket.accept()
-    buf = connection.recv(64)
-    if len(buf) > 0:
-        print (buf)
-        break
+    # buf.append(connection.recv(64))
+    while True:
+        print("{}".format(connection.recv(16)))
+        # print(connection.recv(16))
+    # if len(buf) > 0:
+    #     print (buf)
+    #     break
