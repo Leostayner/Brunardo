0fd1b35987acd04f54c4c5eb15d325f546989007
Robotica Computacional <leostayner@gmail.com>
2017-10-24 12:09:29 -0200

    interface, update receptor

--- 8-Modulacao-Digital/src/Interface.py
+++ 8-Modulacao-Digital/src/Interface.py
@@ -1,8 +1,9 @@
+#Criado por Leonardo Medeiros
 import tkinter as tk
 import time
 from datetime import datetime
 import tkinter.messagebox as tkm
-from PIL import ImageTk
+#from PIL import ImageTk
 from PIL import Image
 from tkinter import filedialog
 
@@ -16,20 +17,20 @@ class Janela_Principal():
         self.app_config['window_xpos'] = 100
         self.app_config['window_ypos'] = 100
         
-        window_width  = self.app_config['width_button'] * 1
-        window_height = self.app_config['height_button'] * 3
+        self.window_width  = self.app_config['width_button'] * 2
+        self.window_height = self.app_config['height_button'] * 3
 
         self.window = tk.Tk()
-        self.window.geometry("{}x{}+{}+{}".format(window_width, 
-                                                  window_height, 
+        self.window.geometry("{}x{}+{}+{}".format(self.window_width, 
+                                                  self.window_height, 
                                                   self.app_config['window_xpos'], 
                                                   self.app_config['window_ypos']))
         self.window.title("BruNardo")
         self.window.resizable(False, False)
     
         # Geometria da pagina
-        self.window.rowconfigure(0, minsize = window_height)
-        self.window.columnconfigure(0, minsize = window_height)
+        self.window.rowconfigure(0, minsize = self.window_height)
+        self.window.columnconfigure(0, minsize = self.window_height)
         
         # Menu Principal
         self.menu_principal = Menu_Principal(self)
@@ -66,17 +67,17 @@ class Menu_Principal():
         self.window1.rowconfigure(1, minsize = self.janela_principal.app_config['height_button'])
         self.window1.rowconfigure(2, minsize = self.janela_principal.app_config['height_button'])
 
-        self.window1.columnconfigure(0, minsize = self.janela_principal.app_config['width_button'])
+        self.window1.columnconfigure(0, minsize = self.janela_principal.window_width)
                 
         #self.Logo = ImageTk.PhotoImage(Image.open("./interface_imgs/Logo.png"))
         self.Logo_label = tk.Label(self.window1)
-        self.Logo_label.grid(row = 0, column = 0, sticky = "nsew",height = 4, width = 30)
+        self.Logo_label.grid(row = 0, column = 0, sticky = "nsew")
         
-        self.button_Emissor = tk.Button(self.window1, text = "Enviar Texto",background = "blue", height = 4, width = 30)        
+        self.button_Emissor = tk.Button(self.window1,height = 4, width = 10, text = "Enviar Texto")        
         self.button_Emissor.grid(row = 1, column = 0)
         self.button_Emissor.configure(command = self.rodaEmissor)
 
-        self.button_Receptor = tk.Button(self.window1, text = "Receber Texto",background = "blue", height = 4, width = 30)        
+        self.button_Receptor = tk.Button(self.window1, height = 4, width = 10, text = "Receber Texto")        
         self.button_Receptor.grid(row = 2, column = 0)
         self.button_Receptor.configure(command = self.rodaReceptor)
 
@@ -101,7 +102,7 @@ class Emissor_window():
         self.window2.columnconfigure(0)
 
     def mostrar(self):
-        self.janela_principal.mostrar_emissor()
+        self.window2.tkraise()
 
 class Receptor_window():
     
@@ -116,7 +117,7 @@ class Receptor_window():
         self.window3.columnconfigure(0)
 
     def mostrar(self):
-        self.janela_principal.mostrar_receptor()
+        self.window3.tkraise()
 
 app = Janela_Principal()
 app.iniciar()
--- 8-Modulacao-Digital/src/Receptor.py
+++ 8-Modulacao-Digital/src/Receptor.py
@@ -1,17 +1,50 @@
+# Camada Física da Computação
+# Exemplo socket server 
+## https://pymotw.com/2/socket/tcp.html
+
 import socket
+import sys
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 1235))
-serversocket.listen(1) 
+def main():
+    PORTA = 1234
 
-buf = []
+    print("Inicializando socket TCP/IP")
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-while True:
-    connection, address = serversocket.accept()
-    # buf.append(connection.recv(64))
+    # Bind the socket to the port
+    server_address = ('localhost', PORTA)
+    print("PORTA {}".format(PORTA))
+    sock.bind(server_address)
+
+    # Listen for incoming connections
+    sock.listen(1)
+
     while True:
-        print("{}".format(connection.recv(16)))
-        # print(connection.recv(16))
-    # if len(buf) > 0:
-    #     print (buf)
-    #     break
+        # Wait for a connection
+        print("waiting for a connection")
+        connection, client_address = sock.accept()
+
+        try:
+            print(" connection from {}".format(client_address))
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(16)
+                data = byToString(data)
+                print("{}".format(data),end="")
+                if(len(data) <= 0):
+                    break
+
+        finally:
+            # Clean up the connection
+            connection.close()
+
+
+## Convet para String.
+def byToString(value):
+    return value.decode() 
+
+
+if __name__ == "__main__":
+    main()
