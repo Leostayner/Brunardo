3d50a6f3fee2a89f5993e9a92bb18fbdd1057b35
Leostayner <leostayner@gmail.com>
2017-11-06 16:11:47 -0200

    commit inicial

---
+++ 9-COM-Serializacao/README.md
@@ -0,0 +1,10 @@
+﻿---
+title: Camada Física -  APS 8 - Modulação Digital
+author: Leonardo Medeiros e Bruna Kimura - leonardopm3@al.insper.edu.br e brunamk@al.insper.edu.br
+date: Outubro - 2017
+---
+
+
+# Projeto 3 - Modulação Digital
+Projeto 3 Camada Física - 2017.2
+
---
+++ 9-COM-Serializacao/src/sketch_Arduino2_Camada/sketch_Arduino2_Camada.ino
@@ -0,0 +1,24 @@
+/**
+ * Arduino-2 : Recepção RX
+ */
+ // stores received byte
+
+void setup() {
+  Serial.begin(115200);   
+  Serial1.begin(9600,SERIAL_8O2);   
+}
+
+void loop() {
+ test_receive();
+}
+
+void test_write() {
+}
+
+void test_receive() {
+  byte rx_byte = 0;        
+  if (Serial1.available()){
+    rx_byte = Serial1.read();
+    Serial.write(rx_byte);
+  }           
+}
