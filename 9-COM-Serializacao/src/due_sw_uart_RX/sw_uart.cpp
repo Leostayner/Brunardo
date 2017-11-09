b04e28f6b28d163d50f33e37b0147a12d858af0f
Leostayner <leostayner@gmail.com>
2017-11-09 18:26:31 -0200

    ajustes finais

--- 9-COM-Serializacao/src/due_sw_uart_RX/sw_uart.cpp
+++ 9-COM-Serializacao/src/due_sw_uart_RX/sw_uart.cpp
@@ -13,7 +13,7 @@ void sw_uart_setup(due_sw_uart *uart, int rx, int tx, int stopbits, int databits
 }
 
 void sw_uart_write_data(due_sw_uart *uart, char* bufferData, int writeN) {
-  for(int i = 0; i < writeN; i++) {
+  for (int i = 0; i < writeN; i++) {
     sw_uart_write_byte(uart, bufferData[i]);
   }
 }
@@ -23,20 +23,17 @@ void sw_uart_write_string(due_sw_uart *uart, char* stringData) {
 }
 
 int calc_even_parity(char data) {
-
   int Sum = 0;
-  for(int i = 0; i <= 7; i++){
-<<<<<<< HEAD
-    Sum += (data >> i) & 0x01;   
+  for (int i = 0; i <= 7; i++) {
+    Sum += (data >> i ) & 0x01;
+    
   }
-  
-  int paridade = Sum % 2;
-  
-  if (paridade == 0){
-   return 1; 
+  if (Sum % 2 == 0) {
+    return 1;
   }
-  else{
-    return 0;}
+  else {
+    return 0;
+  }
 }
 
 // recebimento de dados da serial
@@ -44,68 +41,59 @@ int sw_uart_receive_byte(due_sw_uart *uart, char* data) {
 
   // variavel para recebimento de dados
   char nchar  = 0;
-  
+
   // variavel para calculo da paridade
   char parity, rx_parity;
   
-  // aguarda start bit
   boolean vericidade = false;
-  while(vericidade == false){
-  
-  // Confirma start BIT
-    if (digitalRead(uart->pin_rx) == 0 ){
-      _sw_uart_wait_half_T(uart);
-<<<<<<< HEAD
-      
-=======
+  // aguarda start bit
+  while(!vericidade){
     
->>>>>>> f21801857d9adf0a810b7ad30feb0661a57dc923
-  
-  // checa se bit ainda é 0
-      if(digitalRead(uart->pin_rx) == 0 ){
+    // Confirma start BIT
+    if (digitalRead(uart -> pin_rx) == 0){
+      _sw_uart_wait_half_T(uart);
+      
+      // checa se bit ainda é 0
+      if(digitalRead(uart -> pin_rx) == 0){
         vericidade = true;
-        }  
+      }
     }
   }
-  _sw_uart_wait_T(uart);
   
+  _sw_uart_wait_T(uart);
+
   // recebe dados
-  for(int i = 0 ; i < 7; i++){
-    nchar = nchar | (digitalRead(uart->pin_rx) << i);  
-    _sw_uart_wait_T(uart);
+  for (int i = 0; i <= 7; i++){
+    nchar = nchar|(digitalRead(uart -> pin_rx) << i);
+    _sw_uart_wait_T(uart);  
   }
-  
+
   // recebe paridade
-  rx_parity = digitalRead(uart->pin_rx);
+  rx_parity = digitalRead(uart -> pin_rx);
   _sw_uart_wait_T(uart);
-
+    
   // recebe stop bit
-  int stop = digitalRead(uart->pin_rx);
-<<<<<<< HEAD
-  _sw_uart_wait_T(uart);
-=======
-//  _sw_uart_wait_T(uart);
->>>>>>> f21801857d9adf0a810b7ad30feb0661a57dc923
+  int stopBit = digitalRead(uart -> pin_rx);
 
+  // checa paridade
   parity = calc_even_parity(nchar);
   
-  // checa paridade
-  if(parity != rx_parity) {
+  if (parity != rx_parity) {
     return SW_UART_ERROR_PARITY;
   }
-  
+
   *data = nchar;
   return SW_UART_SUCCESS;
 }
 
 void sw_uart_write_byte(due_sw_uart *uart, char data) {
- 
+
 }
 
 // MCK 21MHz
 // 1093 para baudrate 9600/2
 void _sw_uart_wait_half_T(due_sw_uart *uart) {
-  for(int i = 0; i < 1093; i++)
+  for (int i = 0; i < 1093; i++)
     asm("NOP");
 }
 
@@ -113,4 +101,3 @@ void _sw_uart_wait_T(due_sw_uart *uart) {
   _sw_uart_wait_half_T(uart);
   _sw_uart_wait_half_T(uart);
 }
-
